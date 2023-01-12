c27becd74d850300f174c59b5783e9347167dfd9
ibibeklamichhane <bibeklc714@gmail.com>
2023-01-12 09:46:45 +0545

    Addedupload and result page

--- api/predictor/predict.py
+++ api/predictor/predict.py
@@ -2,12 +2,14 @@ def predict_gen(meta1):
     import pickle
     import os
     from django.conf import settings
-    path = os.path.join(settings.MODELS, 'models.pkl')
+    path = os.path.join(settings.MODELS, 'model.pkl')
     with open(path, 'rb') as pickled:
         data = pickle.load(pickled)
-   # svmp = data['svmp']
-   # norma = data['norma']
-   # lgn = data['lgn']
-   # x = norma.transform([meta1])
+  # # svmp = data['svmp']
+  #     norma = data['norma']
+  # # lgn = data['lgn']
+  #  x = norma.transform([meta1])
     pred = data.predict(meta1)
     return([pred[0]])
+  
+  
--- api/predictor/views.py
+++ api/predictor/views.py
@@ -1,5 +1,4 @@
 
-
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
